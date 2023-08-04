4a50588dd99f4125aa9e6016ac618ee0f980088a
Gabrielle <gabrielleajornalist@gmailcom>
2023-08-04 19:36:44 -0300

    Exercícios

--- exemplo3 Exercício5.js
+++ exemplo3 Exercício5.js
@@ -1,15 +1,17 @@
 function calculaPesoIdeal() {
-  const altura = parseFloat (document.getElementById("altura").value);
-  const sexo = document.getElementById("masc").value;
-
-  let pesoIdealIdeal
-  if (sexo == "M"){ //ex: 5 == '5' true; 5 === '5' - false
-    pesoIdeal=72.7 * altura - 58;
-  } else {
+  const altura = parseFloat(document.getElementById("altura").value);
+  const sexoM = document.getElementById("masc");
+  const sexoF = document.getElementById("fem");
+  let pesoIdeal
+//ex: 5 == '5' true; 5 === '5' - false
+  if (sexoM.checked) { 
+    pesoIdeal = 72.7 * altura - 58;
+  } 
+  if(sexoF.checked) {
     pesoIdeal = 62.1 * altura - 44;
   }
   
 
-  document.getElementById("result").innerHTML= pesoIdeal;
+  document.getElementById("result").innerHTML= pesoIdeal.toFixed(2);
   
 }
