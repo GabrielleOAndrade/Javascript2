46fb0e65e9714f5d043412eaa7cba3895245dfae
Gabrielle <gabrielleajornalist@gmailcom>
2023-08-08 21:51:24 -0300

    Exemplo 4

--- exemplo4.js
+++ exemplo4.js
@@ -1,20 +1,41 @@
-function verificar() {
-  const palavra = document.getElementById("palavra").value.toLowerCase();
+function cadastrar() {
+  const nome = document.getElementById("nome").value;
   //ex: 5 == '5' true; 5 === '5' - false
   // typeof(palavra) = verifica o tipo de yna variável constante
-  if (palavra == "") {
-    document.getElementById("result").innerHTML =
-      "NÃO FOI INFORMADA NENHUMA PALAVRA";
-  } else {
-    const inversa = palavra.split("").reverse().join("");
+  const idade = document.getElementById("idade").value;
+  const email = document.getElementById("email").value;
 
-    if (palavra == inversa) {
-      document.getElementById("result").innerHTML = "SIM";
-    } else {
-      document.getElementById("result").innerHTML = "NÃO";
-    }
-  }
-}
-function limpar() {
+  const pessoa ={
+    nome: nome,
+    idade: idade,
+    email: email,
+  };
+  document.getElementById('result').innerHTML = pessoa.nome + '<br>'
+  + pessoa.idade + "<br>" + pessoa.email;
+ };
+
+ const telefones = ["64 9 9987-4100", "62 9 9654-7845"]
+ 
+ const produtos = [
+  { nome:"TV", quantidae: 10, preco: 2500.78},
+  { nome:"celular", quantidade: 5, preco: 2900.45 }
+  { nome: "Notebook", quantidade: 3, preco: 3500.89 }
+  { nome: "Computador", quantidade: 10, preco: 2871.45 }
+
+ ];
+
+ document.getElementById("result").innerHTML =
+ pessoa.nome +
+ "<br>" +
+ pessoa.idade +
+ "<br>" +
+ pessoa.email +
+ "<br>" +
+ telefones [0] +
+ "<br>" +
+ produtos[2].nome;
+
+
+ function limpar() {
   document.getElementById("result").innerHTML = "";
 }
